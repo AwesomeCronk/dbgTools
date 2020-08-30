1f8a28718a096452fc0dba15d0993e66e0a63e23
Clayton Cronk <awesomecronk@gmail.com>
2020-08-30 08:13:27 -0500

    Added a clean exit to the context manager.

--- cleanup.py
+++ cleanup.py
@@ -1,27 +1,39 @@
 import traceback
 class cleaner():
-    def __init__(self, holder, action = None, args = (), printer = print):
+    def __init__(self, holder, okAction = None, okArgs = (), badAction = None, badArgs = (), printer = print):
         self.holder = holder
-        self.action = action
-        self.actionArgs = args
+        self.okAction = okAction
+        self.okArgs = okArgs
+        self.badAction = badAction
+        self.badArgs = badArgs
         self.printer = printer
         self.stopped = False
-        return
+        self.excTF = False
     def __enter__(self):
-        return
+        pass
     def __exit__(self, *args):
+        #Get the string equivalent of the exception
         excStrs = traceback.format_exception(*args)
         excPrintStr = '\n'
         for excStr in excStrs:
             excPrintStr += excStr
-        self.printer("Exception in {}. Printing traceback:{}".format(self.holder, excPrintStr))
-        if not self.stopped:
-            if self.action != None:
+        #Check if exception actually occurred or if manager is just exiting
+        if excPrintStr != '\nNoneType: None\n':
+            self.excTF = True
+        #Take action based on the situation
+        if not self.stopped and self.excTF:
+            self.printer("Exception in {}. Printing traceback:{}".format(self.holder, excPrintStr))
+            if self.badAction != None:
                 self.printer("Cleaning up after exception in {}.".format(self.holder))
-                self.action(*self.actionArgs)
+                self.badAction(*self.badArgs)
             else:
                 self.printer("No cleanup action specified for {}.".format(self.holder))
             self.stopped = True
+        elif not self.stopped and not self.excTF:
+            self.printer('Context manager in {} exiting. No exceptions occurred.'.format(self.holder))
+            if self.okAction != None:
+                self.okAction(*self.okArgs)
+            else:
+                self.printer('No clean exit action specified for {}.'.format(self.holder))
         else:
             self.printer("Cleanup has already occured.")
-        return
